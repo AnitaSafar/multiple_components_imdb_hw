6c64a47a0389d10eed07ba5cb2e538248254ff35
Anita Safar <ancsa0416@gmail.com>
2021-11-22 19:58:40 +0000

    data updated, made data dynamic, key added to Node, children added to Film

--- imdb/src/components/FilmList.js
+++ imdb/src/components/FilmList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Film from "./Film"
 
-const FilmList = () => {
+const FilmList = ({films}) => {
+
+const filmNodes = films.map(film => {
     return(
-        <>
-            <Film/>
-        </>
+        <Film name={film.name} key={film.id}>{film.url}</Film>
+    );
+});
+
+return(
+    <>
+        {filmNodes}
+    </>
     );
 };
 
--- imdb/src/containers/FilmReleases.js
+++ imdb/src/containers/FilmReleases.js
@@ -7,27 +7,28 @@ const FilmReleases = () => {
         [
             {
               id: 1,
-              name: "Spider-Man: Into the Spider-Verse",
-              url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+              name: "Encanto",
+              url: "https://www.imdb.com/title/tt2953050/?ref_=rlm"
             },
             {
               id: 2,
-              name: "Life Itself",
-              url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+              name: "Dying to Divorce",
+              url: "https://www.imdb.com/title/tt13234764/?ref_=rlm"
             },
             {
               id: 3,
-              name: "Mary Queen of Scots",
-              url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+              name: "House of Gucci",
+              url: "https://www.imdb.com/title/tt11214590/?ref_=rlm"
             },
             {
               id: 4,
-              name: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+              name: "A Boy Called Christmas", 
+              url: "https://www.imdb.com/title/tt10187208/?ref_=rlm"
             },
             {
               id: 5,
-              name: "Captain Marvel",
-              url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+              name: "The First Wave",
+              url: "https://www.imdb.com/title/tt13391444/?ref_=rlm"
             }
           ]
     )
